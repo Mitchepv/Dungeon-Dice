6dd4ab813c6ad06e65305f125b69d6fae413728e
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-02-09 13:09:23 -0500

    Initial Commit

---
+++ Dungeon Dice/Dungeon_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dungeon_DiceApp.swift
+//  Dungeon Dice
+//
+//  Created by Nia Mitchell on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Dungeon_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
