ae3aed929977db479876424235270f1cc75ab384
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-02-09 15:06:09 -0500

    chapter 4 lesson 1: Dungeon Dice

--- Dungeon Dice/ContentView.swift
+++ Dungeon Dice/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = "Roll a dice"
+   
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 50) {
+           
+            Text ("Dungeon Dice!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.red)
+            
+            Spacer()
+            
+           Text(message)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            
+            Group{
+                HStack {
+                    Button ("4-sided") {
+                        rollDice(Side: 4)
+                    }
+                    
+                    Button ("6-sided") {
+                        rollDice(Side: 6)
+                    }
+                    
+                    Button ("8-sided") {
+                        rollDice(Side: 8)
+                    }
+        
+                }
+                
+                HStack {
+                    Button ("10-sided") {
+                        rollDice(Side: 10)
+                    }
+                    
+                    Button ("12-sided") {
+                        rollDice(Side: 12)
+                    }
+                    Button ("20-sided") {
+                        rollDice(Side: 20)
+                    }
+                    
+                }
+                
+                Button ("100-sided") {
+                    rollDice(Side: 100)
+                    }
+             
+            }
+            .font(.title2)
+            .buttonStyle(.glassProminent)
+            .tint(.red)
+            
+        
         }
         .padding()
+    }
+    func rollDice (Side:Int) {
+        let result = Int.random(in: 1...Side)
+        message = "You rolled a \(result) on a \(Side)-sided die"
     }
 }
 
